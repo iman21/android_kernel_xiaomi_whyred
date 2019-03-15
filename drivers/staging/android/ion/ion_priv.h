d9a957e3dbae102a60581e4b0fc92d2cbb47e542
Danny Lin <danny@kdrag0n.dev>
2019-03-14 19:20:39 -0700

    ion: Check out to Android Q DP1 base
    Tag: android-q-preview-1_r0.3
    
    The following commits remain:
    26822f6760272 ion: msm_ion: fix incorrect ion_buffer member assignment
    e0635a289980e ion: Force vmalloc for high-order allocations
    fcf003cc4bd7a ion: add movability support for page pools
    49070595cb848 ion: Do not do allocation if the size exceeding heap remaining
    273ee2c131cc4 ion: correct the behaviour when allocating non-contig regions failed
    593e3c9fe337d ion: fix the return value of shrinkers
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/staging/android/ion/ion_priv.h
+++ drivers/staging/android/ion/ion_priv.h
@@ -578,11 +578,11 @@ void ion_pages_sync_for_device(struct device *dev, struct page *page,
 		size_t size, enum dma_data_direction dir);
 
 int ion_walk_heaps(struct ion_client *client, int heap_id,
-			enum ion_heap_type type, void *data,
-			int (*f)(struct ion_heap *heap, void *data));
+		   enum ion_heap_type type, void *data,
+		   int (*f)(struct ion_heap *heap, void *data));
 
-struct ion_handle *ion_handle_get_by_id_nolock(struct ion_client *client,
-					       int id);
+struct ion_handle *ion_handle_get_by_id(struct ion_client *client,
+					int id);
 
 int ion_handle_put(struct ion_handle *handle);
 
