7977b6e0acc6b8375447a61c1f4c3c02fda09531
Pzqqt <821026875@qq.com>
2019-08-19 09:04:20 +0800

    build.sh: built with clang 9.0.6

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ Start=$(date +"%s")
 export ARCH=arm64
 export SUBARCH=arm64
 export HEADER_ARCH=arm64
-export CLANG_PATH=/home/pzqqt/bin/android_prebuilts_clang_host_linux-x86_clang-5696680
+export CLANG_PATH=/home/pzqqt/bin/android_prebuilts_clang_host_linux-x86_clang-5799447
 export KBUILD_COMPILER_STRING=$($CLANG_PATH/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 
 export KBUILD_BUILD_HOST="lenovo"
