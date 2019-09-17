8d0a7bfe08cff39580dfcefba55845dfaccea1b5
Pzqqt <821026875@qq.com>
2019-09-17 09:13:54 +0800

    build.sh: v2.4

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ red='\033[0;31m'
 gre='\e[0;32m'
 ZIMG=./out/arch/arm64/boot/Image.gz-dtb
 
-export LOCALVERSION=-v2.3
+export LOCALVERSION=-v2.4
 if $use_llvm; then
 	export LOCALVERSION=${LOCALVERSION}-llvm
 else
