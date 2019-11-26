d8b6f7eb5b241894fcf8e6d6cd612e94fbd219c4
Pzqqt <821026875@qq.com>
2019-11-26 09:57:57 +0800

    build.sh: v3.5

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ red='\033[0;31m'
 gre='\e[0;32m'
 ZIMG=./out/arch/arm64/boot/Image.gz-dtb
 
-export LOCALVERSION=-v3.4
+export LOCALVERSION=-v3.5
 export LOCALVERSION="-Q"${LOCALVERSION}
 
 rm -f $ZIMG
