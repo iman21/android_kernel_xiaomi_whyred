cbaa849783265ccf6a371d8c8c8e9c5cf22cbc0e
Pzqqt <821026875@qq.com>
2019-12-31 22:11:15 +0800

    Add campatch.patch & update build.sh
    Follow commit: 2f0bd3a0ccb2ef572e7c58d0517b3c83278274e6

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ mkdtbs=false
 oc_flag=false
 uv_flag=false
 more_uv_flag=false
+campatch_flag=false
 
 for arg in $@; do
 	case $arg in
@@ -19,6 +20,7 @@ for arg in $@; do
 		"-oc") oc_flag=true;;
 		"-40uv") uv_flag=true;;
 		"-80uv") more_uv_flag=true;;
+		"-campatch") campatch_flag=true;;
 	esac
 done
 
@@ -44,6 +46,7 @@ export KBUILD_BUILD_USER="pzqqt"
 $oc_flag && { git apply ./oc.patch || exit 1; }
 $uv_flag && { git apply ./40mv_uv.patch || exit 1; }
 $more_uv_flag && { git apply ./80mv_uv.patch || exit 1; }
+$campatch_flag && { git apply ./campatch.patch || exit 1; }
 
 $disable_mkclean || make mrproper O=out || exit 1
 make whyred-perf_defconfig O=out || exit 1
@@ -73,6 +76,7 @@ Diff=$(($End - $Start))
 $oc_flag && { git apply -R ./oc.patch || exit 1; }
 $uv_flag && { git apply -R ./40mv_uv.patch || exit 1; }
 $more_uv_flag && { git apply -R ./80mv_uv.patch || exit 1; }
+$campatch_flag && { git apply -R ./campatch.patch || exit 1; }
 
 if $mkdtbs; then
 	if [ $exit_code -eq 0 ]; then
